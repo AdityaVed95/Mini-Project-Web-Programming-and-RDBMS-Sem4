7a7d09f85392b553e20ceb58a46cb6abd75e6b7b
Abhishek gupta <07abhishekgupta07@gmail.com>
2023-04-18 01:38:04 +0530

    frontend updated.

--- flask_application_final/app.py
+++ flask_application_final/app.py
@@ -13,7 +13,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-@app.route('/welcome')
+@app.route('/')
 def welcome_fxn():
     temp = request.headers.get('User-Agent')
     if "iPhone" in temp or "Android" in temp:
